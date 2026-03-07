de140bea07b207564a2770749dc46a4c75f74c31
y-izutsu <62274269+y-izutsu@users.noreply.github.com>
2026-03-08 02:49:45 +0900

    jsファイルに変更

--- public/utils/ga4.js
+++ public/utils/ga4.js
@@ -1,13 +1,3 @@
-export {};
-
-type Gtag = (...args: unknown[]) => void;
-
-declare global {
-  interface Window {
-    gtag?: Gtag;
-  }
-}
-
 document.addEventListener('astro:page-load', () => {
   window.gtag?.('event', 'page_view', {
     page_location: window.location.href,
