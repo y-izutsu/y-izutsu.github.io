63d1542d1f496a4faa58478b227110a9de19aba9
y-izutsu <62274269+y-izutsu@users.noreply.github.com>
2026-03-08 03:03:27 +0900

    ページのフルロードに変更

--- public/utils/ga4.js
+++ public/utils/ga4.js
@@ -1,7 +1,9 @@
-document.addEventListener('astro:page-load', () => {
+document.addEventListener('load', () => {
+  console.log('GA4 page_view 発火準備OK');
   window.gtag?.('event', 'page_view', {
     page_location: window.location.href,
     page_path: window.location.pathname,
     page_title: document.title,
   });
+  console.log('GA4 page_view 送信したよ');
 });
